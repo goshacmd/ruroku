b07524ea39a14a88fd9050a6a0d823c29044e216
Gosha Arinich <me@goshakkk.name>
2012-06-10 23:50:37 +0300

    typo fix [ci skip]

--- lib/ruroku/addon_set.rb
+++ lib/ruroku/addon_set.rb
@@ -9,7 +9,7 @@ module Ruroku
     #
     #   addons.add 'addon-name'
     #   addons.upgrade 'addon-name'
-    #   addon.delete 'addon-name'
+    #   addons.delete 'addon-name'
     map_api add: :post_addon,
       upgrade: :put_addon,
       delete: :delete_addon
